cbb5ffe08fd74d821a7918fb732add0c795b769c
sharafudheen.M <sharafumt.hidragen@gmail.com>
2025-02-08 21:28:37 +0530

    Fix Prisma binary target issue for Vercel

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -7,21 +7,6 @@ import {
 import { setContext } from "@apollo/client/link/context";
 import { getSession } from "next-auth/react";
 
-// const getApiUrl = () => {
-//   if (process.env.NEXT_PUBLIC_API_URL) {
-//     return process.env.NEXT_PUBLIC_API_URL;
-//   }
-//   if (typeof window !== "undefined") {
-//     const protocol = window.location.protocol;
-//     const host = window.location.host;
-//     return `${protocol}//${host}/api/graphql`;
-//   }
-
-//   return process.env.VERCEL_URL
-//     ? `${process.env.VERCEL_URL}/api/graphql`
-//     : "http://localhost:8000/api/graphql";
-// };
-
 const httpLink = createHttpLink({
   uri: "https://blog-management-lilac.vercel.app/api/graphql",
 });
